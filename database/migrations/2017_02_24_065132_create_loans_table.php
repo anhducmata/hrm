71a890330828907355f5ab3125f149d1ddd6856c
anhducmata <anhducmata@gmail.com>
2017-02-24 15:17:31 +0700

    DB design Migration

--- database/migrations/2017_02_24_065132_create_loans_table.php
+++ database/migrations/2017_02_24_065132_create_loans_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePayrollTable extends Migration
+class CreateLoansTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,18 +13,17 @@ class CreatePayrollTable extends Migration
      */
     public function up()
     {
-        Schema::create('payroll', function (Blueprint $table) {
+        Schema::create('loans', function (Blueprint $table) {
             $table->increments('id');
             $table->string('user_id');
-            $table->string('deductions');
-            $table->string('bonuses');
-            $table->string('loans');
-            $table->string('insurance');
-            $table->string('advance_salary');
-            $table->string('overtimes');
-            $table->string('salary_payslips');
-            $table->string('commissions');
-            $table->string('reimbursements');
+            $table->string('title');
+            $table->string('loan_date');
+            $table->string('repayment_start_date');
+            $table->string('amount');
+            $table->string('monthly_of_interest'); // tiền lãi hàng tháng (%)
+            $table->string('remaining_amount');
+            $table->string('approval_status');
+            $table->string('description');
             $table->timestamps();
         });
     }
